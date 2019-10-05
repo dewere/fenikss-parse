e1df3e5b3b79c79aefd265e7fe7c1a2c270f94ae
ᴅᴇᴡᴇʀᴇ <45142505+dewere@users.noreply.github.com>
2019-10-05 23:38:10 +0300

    Fixed classifiers

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     install_requires=[],
     classifiers=[
-        'Development Status :: 5 - Stable',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
